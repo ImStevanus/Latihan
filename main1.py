1bf9b7005bd69ee463d1303cbb7c03c540d50a0f
ImStevanus <s38250029@student.ubm.ac.id>
2026-05-19 15:23:46 +0700

    Update main1.py

--- main1.py
+++ main1.py
@@ -80,4 +80,4 @@ if st.button('🖩 Hitung Hasil'):
     if error_pembagian:
         st.error("Error: Tidak bisa melakukan pembagian dengan angka nol (0)!")
     else:
-        st.success(f"Hasil: {string_proses} = **{hasil}**")
+        st.success(f"Hello World!")
