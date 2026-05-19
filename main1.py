09727bfcf80b6bb8d6cf73a9ba7c9c3afc2545ec
ImStevanus <s38250029@student.ubm.ac.id>
2026-05-19 15:06:52 +0700

    Update main1.py

--- main1.py
+++ main1.py
@@ -16,5 +16,25 @@ st.markdown("---")
 mode = st.selectbox('Pilih mode :', ['Penjumlahan', 'Pengurangan', 'Perkalian', 'Pembagian'])
 st.write('Mode: ', mode)
 
-c1 = st.text_input('Masukkan Angka 1')
-c2 = st.text_input('Masukkan Angka 2')
+c1 = st.number_input('Masukkan Angka 1', value=0.0)
+c2 = st.number_input('Masukkan Angka 2', value=0.0)
+
+if st.button('Hitung Hasil'):
+    if operasi == 'Penjumlahan (+)':
+        hasil = num1 + num2
+        st.success(f'Hasil dari {num1} + {num2} = **{hasil}**')
+        
+    elif operasi == 'Pengurangan (-)':
+        hasil = num1 - num2
+        st.success(f'Hasil dari {num1} - {num2} = **{hasil}**')
+        
+    elif operasi == 'Perkalian (x)':
+        hasil = num1 * num2
+        st.success(f'Hasil dari {num1} x {num2} = **{hasil}**')
+        
+    elif operasi == 'Pembagian (/)':
+        if num2 == 0:
+            st.error('Error: Tidak bisa membagi angka dengan nol (0)!')
+        else:
+            hasil = num1 / num2
+            st.success(f'Hasil dari {num1} / {num2} = **{hasil}**')
